853052276ac60556aacf816919949624bf59171a
BeokBeok <kekemusa37@nate.com>
2020-11-18 15:06:06 +0900

    [추가] - SerialName in Coordinates

--- app/src/main/kotlin/com/beok/randomuser/data/Coordinates.kt
+++ app/src/main/kotlin/com/beok/randomuser/data/Coordinates.kt
@@ -1,9 +1,12 @@
 package com.beok.randomuser.data
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class Coordinates(
+	@SerialName("latitude")
 	val latitude: String = "",
+	@SerialName("longitude")
 	val longitude: String = ""
 )
