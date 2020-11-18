3527ff6e9524f13f278697e4bb9d7a7187b4b43e
BeokBeok <kekemusa37@nate.com>
2020-11-18 14:56:09 +0900

    [제거] - CoordinatesTest 에서 Location

--- app/src/test/kotlin/com/beok/randomuser/data/CoordinatesTest.kt
+++ app/src/test/kotlin/com/beok/randomuser/data/CoordinatesTest.kt
@@ -8,33 +8,18 @@ import org.junit.jupiter.api.Test
 class CoordinatesTest {
 
     @Test
-    fun `Location 의 Coordinates 관련 Json 을 엔티티로 변환합니다`() {
-        val response = Json.decodeFromString<Location>(LOCATION_JSON)
+    fun `Coordinates 관련 Json 을 엔티티로 변환합니다`() {
+        val response = Json.decodeFromString<Coordinates>(COORDINATES_JSON)
 
-        response.coordinates.run {
-            assertThat(latitude).isEqualTo("14.4031")
-            assertThat(longitude).isEqualTo("57.7706")
-        }
+        assertThat(response.latitude).isEqualTo("14.4031")
+        assertThat(response.longitude).isEqualTo("57.7706")
     }
 
     companion object {
-        private const val LOCATION_JSON = """
+        private const val COORDINATES_JSON = """
             {
-                "street":{
-                   "number":8409,
-                   "name":"Mill Road"
-                },
-                "city":"Cambridge",
-                "state":"Cleveland",
-                "country":"United Kingdom",
-                "coordinates":{
-                   "latitude":"14.4031",
-                   "longitude":"57.7706"
-                },
-                "timezone":{
-                   "offset":"0:00",
-                   "description":"Western Europe Time, London, Lisbon, Casablanca"
-                }
+               "latitude":"14.4031",
+               "longitude":"57.7706"
             }
         """
     }
