3d65b4da9c93c83ae2e27833bb6b9d0889110ddb
BeokBeok <kekemusa37@nate.com>
2020-11-19 17:48:15 +0900

    [적용] - moshi in CoordinatesTest

--- app/src/test/kotlin/com/beok/randomuser/data/CoordinatesTest.kt
+++ app/src/test/kotlin/com/beok/randomuser/data/CoordinatesTest.kt
@@ -1,15 +1,27 @@
 package com.beok.randomuser.data
 
-import kotlinx.serialization.decodeFromString
-import kotlinx.serialization.json.Json
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 class CoordinatesTest {
 
+    private val moshi = Moshi.Builder()
+        .add(KotlinJsonAdapterFactory())
+        .build()
+    private lateinit var jsonAdapter: JsonAdapter<Coordinates>
+
+    @BeforeEach
+    fun setup() {
+        jsonAdapter = moshi.adapter(Coordinates::class.java)
+    }
+
     @Test
     fun `Coordinates 관련 Json 을 엔티티로 변환합니다`() {
-        val response = Json.decodeFromString<Coordinates>(COORDINATES_JSON)
+        val response = jsonAdapter.fromJson(COORDINATES_JSON) ?: Coordinates()
 
         assertThat(response.latitude).isEqualTo("14.4031")
         assertThat(response.longitude).isEqualTo("57.7706")
