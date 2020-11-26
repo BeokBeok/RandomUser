50f9669c4f1ea615e6399bacddbb46a59434552c
BeokBeok <kekemusa37@gmail.com>
2020-11-26 23:14:09 +0900

    [추가] - UserItem

---
+++ app/src/main/kotlin/com/beok/randomuser/main/UserItem.kt
@@ -0,0 +1,14 @@
+package com.beok.randomuser.main
+
+import com.beok.randomuser.data.ResultsItem
+
+data class UserItem(val imageUrl: String, val name: String) {
+
+    companion object {
+
+        fun toItem(resultsItem: ResultsItem) = UserItem(
+            imageUrl = resultsItem.picture.thumbnail,
+            name = "${resultsItem.name.title}${resultsItem.name.first}${resultsItem.name.last}"
+        )
+    }
+}
