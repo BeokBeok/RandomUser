bdd44a8af4bb3913be7f65dff56c1687b77ef182
BeokBeok <kekemusa37@gmail.com>
2020-11-26 22:21:34 +0900

    [이동] - MainActivity in main package

--- app/src/main/kotlin/com/beok/randomuser/main/MainActivity.kt
+++ app/src/main/kotlin/com/beok/randomuser/main/MainActivity.kt
@@ -1,8 +1,11 @@
-package com.beok.randomuser
+package com.beok.randomuser.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.beok.randomuser.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
