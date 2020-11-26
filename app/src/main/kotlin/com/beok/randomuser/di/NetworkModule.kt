19606758896ab8abe966fe024815c0edda7653c1
BeokBeok <kekemusa37@gmail.com>
2020-11-26 21:58:47 +0900

    [추가] - NetworkModule

---
+++ app/src/main/kotlin/com/beok/randomuser/di/NetworkModule.kt
@@ -0,0 +1,72 @@
+package com.beok.randomuser.di
+
+import com.beok.randomuser.BuildConfig
+import com.beok.randomuser.domain.RandomUserService
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(ApplicationComponent::class)
+class NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideHttpLoggingInterceptor() = HttpLoggingInterceptor()
+        .apply {
+            level = if (BuildConfig.DEBUG) {
+                HttpLoggingInterceptor.Level.BODY
+            } else {
+                HttpLoggingInterceptor.Level.NONE
+            }
+        }
+
+    @Provides
+    @Singleton
+    fun provideKotlinJsonAdapterFactory() = KotlinJsonAdapterFactory()
+
+    @Provides
+    @Singleton
+    fun provideMoshi(jsonAdapterFactory: KotlinJsonAdapterFactory): Moshi =
+        Moshi.Builder()
+            .add(jsonAdapterFactory)
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideMoshiConverter(moshi: Moshi): MoshiConverterFactory =
+        MoshiConverterFactory.create(moshi)
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(loggingInterceptor: HttpLoggingInterceptor): OkHttpClient =
+        OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(converterFactory: MoshiConverterFactory, client: OkHttpClient): Retrofit =
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(converterFactory)
+            .client(client)
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideService(retrofit: Retrofit): RandomUserService =
+        retrofit.create(RandomUserService::class.java)
+
+    companion object {
+        private const val BASE_URL = "https://randomuser.me/api/"
+    }
+}
