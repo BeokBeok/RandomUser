3980f1b2ac7430aa91d4b7f57a8294126548823f
BeokBeok <kekemusa37@gmail.com>
2020-11-26 23:18:39 +0900

    [구현] - MainViewModel

---
+++ app/src/main/kotlin/com/beok/randomuser/main/MainViewModel.kt
@@ -0,0 +1,25 @@
+package com.beok.randomuser.main
+
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.beok.randomuser.domain.RandomUserRepository
+import kotlinx.coroutines.launch
+
+class MainViewModel @ViewModelInject constructor(
+    private val repository: RandomUserRepository
+) : ViewModel() {
+
+    private val _users = MutableLiveData<List<UserItem>>()
+    val users: LiveData<List<UserItem>> get() = _users
+
+    fun fetchUsers(numberOfUser: Int) = viewModelScope.launch {
+        _users.value = repository.fetchUsers(numberOfUser = numberOfUser)
+            .getOrNull()
+            ?.results
+            ?.map(UserItem::toItem)
+            ?: emptyList()
+    }
+}
