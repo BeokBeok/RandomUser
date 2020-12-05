db0638cd8088d2f7a208699587e725ed74e8afde
BeokBeok <kekemusa37@gmail.com>
2020-12-05 21:27:09 +0900

    [추가] - LocalDataSource in RepositoryModule

--- app/src/main/kotlin/com/beok/randomuser/data/RandomUserRepository.kt
+++ app/src/main/kotlin/com/beok/randomuser/data/RandomUserRepository.kt
@@ -1,6 +1,7 @@
 package com.beok.randomuser.data
 
 import com.beok.randomuser.data.entity.RandomUserResponse
+import com.beok.randomuser.data.source.local.RandomUserLocalDataSource
 import com.beok.randomuser.data.source.remote.RandomUserRemoteDataSource
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
@@ -8,6 +9,7 @@ import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class RandomUserRepository @Inject constructor(
+    private val localDataSource: RandomUserLocalDataSource,
     private val remoteDataSource: RandomUserRemoteDataSource,
     private val ioDispatcher: CoroutineDispatcher = Dispatchers.IO
 ) {
--- app/src/main/kotlin/com/beok/randomuser/di/RepositoryModule.kt
+++ app/src/main/kotlin/com/beok/randomuser/di/RepositoryModule.kt
@@ -1,6 +1,9 @@
 package com.beok.randomuser.di
 
 import com.beok.randomuser.data.RandomUserRepository
+import com.beok.randomuser.data.source.local.RandomUserDao
+import com.beok.randomuser.data.source.local.RandomUserLocalDataSource
+import com.beok.randomuser.data.source.local.RandomUserLocalDataSourceImpl
 import com.beok.randomuser.data.source.remote.RandomUserRemoteDataSource
 import com.beok.randomuser.data.source.remote.RandomUserRemoteDataSourceImpl
 import com.beok.randomuser.data.source.remote.RandomUserService
@@ -16,11 +19,19 @@ class RepositoryModule {
 
     @Provides
     @Singleton
+    fun providesLocalDataSource(randomUserDao: RandomUserDao): RandomUserLocalDataSource =
+        RandomUserLocalDataSourceImpl(randomUserDao)
+
+    @Provides
+    @Singleton
     fun provideRemoteDataSource(service: RandomUserService): RandomUserRemoteDataSource =
         RandomUserRemoteDataSourceImpl(service)
 
     @Provides
     @Singleton
-    fun provideRepository(remoteDataSource: RandomUserRemoteDataSource): RandomUserRepository =
-        RandomUserRepository(remoteDataSource)
+    fun provideRepository(
+        localDataSource: RandomUserLocalDataSource,
+        remoteDataSource: RandomUserRemoteDataSource
+    ): RandomUserRepository =
+        RandomUserRepository(localDataSource, remoteDataSource)
 }
