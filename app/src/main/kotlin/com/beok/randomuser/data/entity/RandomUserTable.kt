dda2e7996e0621587b363466c174d0b9b0021fac
BeokBeok <kekemusa37@gmail.com>
2020-12-05 21:23:11 +0900

    [추가] - primary key

--- app/src/main/kotlin/com/beok/randomuser/data/entity/RandomUserTable.kt
+++ app/src/main/kotlin/com/beok/randomuser/data/entity/RandomUserTable.kt
@@ -1,8 +1,10 @@
 package com.beok.randomuser.data.entity
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity(tableName = "user")
 data class RandomUserTable(
+    @PrimaryKey
     val name: String
 )
