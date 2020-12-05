7b60e79a3c8f0af811fb3b9d7cd2f8906e276e21
BeokBeok <kekemusa37@gmail.com>
2020-12-05 21:02:53 +0900

    [추가] - RandomUserDatabase

---
+++ app/src/main/kotlin/com/beok/randomuser/data/source/local/RandomUserDatabase.kt
@@ -0,0 +1,14 @@
+package com.beok.randomuser.data.source.local
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.beok.randomuser.data.entity.RandomUserTable
+
+@Database(
+    entities = [RandomUserTable::class],
+    version = 1
+)
+abstract class RandomUserDatabase : RoomDatabase() {
+
+    abstract fun randomUserDao(): RandomUserDao
+}
