5873e9a8be765292d65f8777f4cd63159bde87fa
BeokBeok <kekemusa37@gmail.com>
2020-12-05 21:26:10 +0900

    [추가] - exportSchema

--- app/src/main/kotlin/com/beok/randomuser/data/source/local/RandomUserDatabase.kt
+++ app/src/main/kotlin/com/beok/randomuser/data/source/local/RandomUserDatabase.kt
@@ -6,7 +6,8 @@ import com.beok.randomuser.data.entity.RandomUserTable
 
 @Database(
     entities = [RandomUserTable::class],
-    version = 1
+    version = 1,
+    exportSchema = false
 )
 abstract class RandomUserDatabase : RoomDatabase() {
 
