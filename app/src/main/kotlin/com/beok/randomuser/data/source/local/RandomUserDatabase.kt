a9a5f4885b36299240f787edada4152be77f7a8e
BeokBeok <kekemusa37@gmail.com>
2020-12-06 22:27:41 +0900

    [구현] - Migration 1 to 2

--- app/src/main/kotlin/com/beok/randomuser/data/source/local/RandomUserDatabase.kt
+++ app/src/main/kotlin/com/beok/randomuser/data/source/local/RandomUserDatabase.kt
@@ -2,14 +2,30 @@ package com.beok.randomuser.data.source.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.beok.randomuser.data.entity.RandomUserTable
 
 @Database(
     entities = [RandomUserTable::class],
-    version = 1,
-    exportSchema = false
+    version = 2
 )
 abstract class RandomUserDatabase : RoomDatabase() {
 
     abstract fun randomUserDao(): RandomUserDao
+
+    companion object {
+        val MIGRATION_1_2 = object : Migration(1, 2) {
+            override fun migrate(database: SupportSQLiteDatabase) {
+                database.execSQL(
+                    "CREATE TABLE user_2 (name TEXT PRIMARY KEY NOT NULL, gender TEXT NOT NULL)"
+                )
+                database.execSQL(
+                    "INSERT INTO user_2 (name, gender) SELECT name, '' FROM user"
+                )
+                database.execSQL("DROP TABLE user")
+                database.execSQL("ALTER TABLE user_2 RENAME TO user")
+            }
+        }
+    }
 }
